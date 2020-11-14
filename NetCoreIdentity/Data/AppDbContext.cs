0fdd56cf617367e465ce0acda98f02fbb7ecaa2b
JAMEEL ALEDLBI <JAMEL_056@hotmail.com>
2020-11-14 15:15:35 +0300

    setup the appDbContext and register identity in startup

---
+++ NetCoreIdentity/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetCoreIdentity.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
