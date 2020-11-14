e77c1aa2061635b6b1e94a7005ffc37d69b79ebb
JAMEEL ALEDLBI <JAMEL_056@hotmail.com>
2020-11-14 15:21:47 +0300

    add the connection string and register it in startup with downloading the necessary packages

--- NetCoreIdentity/Startup.cs
+++ NetCoreIdentity/Startup.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NetCoreIdentity.Data;
@@ -10,10 +12,23 @@ namespace NetCoreIdentity
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+                {
+                    options.UseSqlServer(_configuration.GetConnectionString("IdentityConnection"));
+                }
+                );
+
             services.AddIdentity<IdentityUser, IdentityRole>(options =>
             {
                 options.Password.RequiredLength = 8;
